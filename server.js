56cf9d4a98e4a247d462ce4d6e17cbdf7f137688
King4Alex <alexandre.bonte@beesearch.fr>
2013-12-09 18:27:13 +0100

    change to sales only

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 
 var express = require('express');
 var customer = require('./routes/customer');
-var album = require('./routes/album');
 var invoice = require('./routes/invoice');
 var http = require('http');
 
@@ -12,16 +11,12 @@ var http = require('http');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3300);
+app.set('port', process.env.PORT || 3301);
 app.use(app.router);
 
 
 // routes
 app.get('/customers', customer.listAllCustomer);
-
-app.get('/albums', album.listAllAlbum);
-app.get('/albumsWithArtistName', album.listAllAlbumWithArtistName);
-app.get('/updateAlbum', album.updateAlbum);
 
 app.get('/invoices', invoice.listAllInvoice);
 app.get('/invoicesWithCustomerName', invoice.listAllInvoiceWithCustomerName);
